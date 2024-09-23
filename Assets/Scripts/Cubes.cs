cbb102d3424345a0a1bfc47f381bbecf03e4454d
Ucv <ucv@ucv.edu.pe>
2024-09-23 09:27:22 -0500

    Add Scripts for use Gravity on Cubes

---
+++ Assets/Scripts/Cubes.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cubes : MonoBehaviour
+{
+
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+
+    private void OnMouseDown()
+    {
+        rb.AddForce(transform.forward * 500f);
+        rb.useGravity = true;
+    }
+}
